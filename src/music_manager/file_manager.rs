f3978d270efb12f2c610e5cdc63d4bd8e213406b
Adrián García Pérez <adrian.garcia04@ciencias.unam.mx>
2018-09-28 22:20:40 -0500

    Non-music files are ignored while searching

--- src/music_manager/file_manager.rs
+++ src/music_manager/file_manager.rs
@@ -1,10 +1,10 @@
 extern crate dirs;
 
-use std::{io, path};
-use std::fs;
+use std::{io, path, fs, fs::DirEntry, slice::Iter};
 
 pub struct FileManager {
     directory: path::PathBuf,
+    music_files: Vec<path::PathBuf>,
 }
 
 impl FileManager {
@@ -22,30 +22,54 @@ impl FileManager {
         };
 
         FileManager {
-            directory: path
+            directory: path,
+            music_files: Vec::new(),
         }
+    }
+
+    pub fn songs(&self) -> Iter<path::PathBuf>{
+        self.music_files.iter()
+    }
+
+    pub fn directory(&self) -> &path::PathBuf {
+        &self.directory
     }
 
     pub fn from_dir(directory: &str) -> FileManager {
         let mut path = path::PathBuf::new();
         path.push(directory);
         FileManager {
-            directory: path
+            directory: path,
+            music_files: Vec::new(),
         }
     }
 
-    pub fn list_songs(&self) {
+    pub fn search_songs(&mut self) -> Result<(), io::Error>{
         match fs::read_dir(&self.directory) {
             Ok(files) => {
                 for file in files {
-                    println!("{:?}", file);
+                    self.save_file(file?);
                 }
+                Ok(())
             },
-            Err(_) => {
-                println!("An error ocurred");
+            Err(e) => {
+                Err(e)
             }
         }
+    }
 
+    fn save_file(&mut self, file: DirEntry) {
+        let file_path_buf = file.path();
+        let file_path_buf_clone = file_path_buf.clone();
+        let file_path = file_path_buf.as_path();
+        match file_path.extension() {
+            Some(extension) => {
+                if extension.eq("mp3") {
+                    self.music_files.push(file_path_buf_clone);
+                }
+            },
+            None => {}
+        }
     }
 }
 
--- src/music_player.rs
+++ src/music_player.rs
@@ -3,9 +3,10 @@ extern crate music_player_rs;
 use music_player_rs::music_manager::{file_manager};
 
 fn main() {
-    let file_manager = file_manager::FileManager::new();
-    file_manager.list_songs();
+    let mut file_manager = file_manager::FileManager::new();
+    file_manager.search_songs().unwrap();
 
-    let file_manager = file_manager::FileManager::from_dir("./test_songs/");
-    file_manager.list_songs();
+    for song in file_manager.songs() {
+        println!("{:?}", song);
+    }
 }
